cc471862f13e457fdbf77ec46e6add9b2623eb04
Ankit180898 <karan180898@gmail.com>
2024-07-05 21:35:47 +0530

    Changes in login screen

--- lib/features/authentication/screens/login/login_screen.dart
+++ lib/features/authentication/screens/login/login_screen.dart
@@ -1,21 +1,61 @@
 import 'package:epic_mart/common/styles/spacing_styles.dart';
 import 'package:epic_mart/utils/constants/image_strings.dart';
 import 'package:epic_mart/utils/constants/sizes.dart';
+import 'package:epic_mart/utils/constants/texts.dart';
+import 'package:epic_mart/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final dark = AppHelperFunctions.isDarkMode(context);
+    return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
           padding: AppSpacingStyles.paddingWithAppBarHeight,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [Image(image: AssetImage(AppImages.darkAppLogo))],
+            children: [
+              Column(
+                children: [
+                  Image(
+                      image: AssetImage(
+                          dark ? AppImages.darkAppLogo : AppImages.lightAppLogo)),
+                  //this will be changed
+                  Text(
+                    AppTexts.homeAppbarTitle,
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  const SizedBox(
+                    height: AppSizes.sm,
+                  ),
+                  Text(
+                    AppTexts.homeAppbarSubTitle,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ],
+              ),
+               Form(
+            child: Column(
+            children: [
+              //Email
+              TextFormField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Iconsax.direct_right),
+                  labelText: AppTexts.email,
+                  
+
+                ),
+              )
+
+            ],
+          ),)
+            ],
           ),
+          
         ),
       ),
     );
--- lib/utils/constants/texts.dart
+++ lib/utils/constants/texts.dart
@@ -11,6 +11,29 @@ class AppTexts {
       "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!";
   static const String onBoardingSubTitle3 =
       "From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery!";
+
+
+ //Authentication Texts
+static const String firstNome = "First Name";
+static const String lastNome = "Last Name";
+static const String email = "E-Mail";
+static const String password = "Password";
+static const String newPassword = "New Password";
+static const String username = "Username";
+static const String phoneNo = "Phone Number";
+static const String rememberHe = "Remember Me";
+static const String forgetPassword = "Forget Password?";
+static const String signin = "Sign In";
+static const String createAccount = "Create Account";
+static const String orSigninwith = "or sign in with";
+static const String orSignUpwith = "or sign up with";
+static const String iAgreeTo = "I agree to";
+static const String privacyPolicy = "Privacy Policy";
+static const String termsOfUse = "Terms of use";
+static const String verificationCode = "verificationCode";
+static const String resendEmoil = "Resend Email";
+static const String resendEmailIn = "Resend email in";
+
 // -- Home
   static const String homeAppbarTitle = "Good day for shopping";
   static const String homeAppbarSubTitle = "Taimoon Sikander";
