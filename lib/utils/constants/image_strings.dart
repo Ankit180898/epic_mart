cc0eac89cb183ba76d1b48638a00f98ec9002e5b
Ankit180898 <karan180898@gmail.com>
2024-07-31 22:57:16 +0530

    Added logo

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -2,8 +2,8 @@ class AppImages {
   AppImages._();
 
   // App logos
-  static const String darkAppLogo = "";
-  static const String lightAppLogo = "";
+  static const String darkAppLogo = "assets/icons/flutter_new.png";
+  static const String lightAppLogo = "assets/icons/flutter_new.png";
 
   // Social Logins
   static const String facebook = "";
